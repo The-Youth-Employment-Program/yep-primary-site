8751a205f5e09dcad7809f718c6396b1d5bde641
Brennen Mazur <specdir@protonmail.ch>
2023-12-23 01:26:26 -0700

    Bugfix: svg path tag element-name fix

--- components/YepHead.js
+++ components/YepHead.js
@@ -26,7 +26,7 @@ export const YepHead = () => {
         </Link>
         <button onClick={toggleIsOpen} type="button" className='ml-2 items-center sm:hidden text-salmon-red hover:text-salmon-green'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="fill-current h-8">
-            {isOpen && <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />}
+            {isOpen && <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />}
             {!isOpen && <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />}
           </svg>
         </button>
