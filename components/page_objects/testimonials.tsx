42425064c5d354b7a70f8dd33ce6207e61828695
Brennen Mazur <specdir@protonmail.ch>
2024-01-09 09:11:22 -0700

    Styles: Bugfix warnings and clean testimonial and action card

--- components/page_objects/testimonials.tsx
+++ components/page_objects/testimonials.tsx
@@ -9,14 +9,17 @@ export function Testimonials(props: PageSectionTestimonials) {
   //should each contain data-tina-field={tinaField(props, "testimonial")}
     return(
       <>
-        <section className="h-96 p-5 bg-salmon-green text-salmon-light-g2">  
+        <section className="grid py-14 px-8 sm:p-10 md:p-20 lg:px-40 items-center content-center bg-salmon-green text-salmon-light-g2">  
           {props.testimonial?.map((testimony,i) => {
             return(
-              <div key={i} data-tina-field={tinaField(props[i],"testimonial")} className="text-lg"> {/*TinaField doesn't work */}
+              <div key={i} data-tina-field={tinaField(props[i],"testimonial")} className="lg:text-2xl sm:text-center"> {/*TinaField doesn't work */}
+                <svg className="w-10 h-10 mx-auto mb-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                  <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                </svg>
                 {testimony?.testimonialQuote}
-                <div className="text-sm text-salmon-light-g1">
-                  {testimony?.testimonialAttribution} - 
-                  {testimony?.testimonialRole}
+                <div className="sm:pt-6 lg:text-md">
+                  {testimony?.testimonialAttribution}  
+                  <span className="text-salmon-light-g1 pl-4">{ "|" }</span><span className="text-salmon-light-g1 pl-4">{testimony?.testimonialRole}</span>
                 </div>
               </div>
             )
