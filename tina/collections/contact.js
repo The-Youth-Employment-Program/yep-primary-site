446fc031595187c8302244abd998915763a99227
Brennen Mazur <specdir@protonmail.ch>
2024-01-10 20:36:26 -0700

    Bugfix: resolve contact page resolution

--- tina/collections/contact.js
+++ tina/collections/contact.js
@@ -6,58 +6,58 @@ export default {
     name:"branchContact",
     path:"content/contact",
     format: "json",
-        fields: [
-            {
-                type: "string",
-                label: "Branch Name",
-                name:"branch",
-                isTitle:true,
-                required:true,
-            },
-            {
-                type:"object",
-                name:"contact",
-                label:"Contact Information",
-                fields:[
-                    {
-                        type:"object",
-                        label:"Address",
-                        name:"address",
-                        fields:[
-                            {
-                                type:"string",
-                                label:"Street Address",
-                                name:"streetAddress"
-                            },
-                            {
-                                type:"string",
-                                label:"City",
-                                name:"ctiy"
-                            },
-                            {
-                                type:"string",
-                                label:"State",
-                                name:"state"
-                            },
-                            {
-                                type:"string",
-                                label:"Zip Code",
-                                name:"zipCode"
-                            }
-                        ]
-                    },
-                    {
-                        type:"string",
-                        label:"Office Phone",
-                        name:"branchPhone"
-                    },
+    fields: [
+        {
+            type: "string",
+            label: "Branch Name",
+            name:"branch",
+            isTitle:true,
+            required:true,
+        },
+        {
+            type:"object",
+            name:"contact",
+            label:"Contact Information",
+            fields:[
+                {
+                    type:"object",
+                    label:"Address",
+                    name:"address",
+                    fields:[
+                        {
+                            type:"string",
+                            label:"Street Address",
+                            name:"streetAddress"
+                        },
+                        {
+                            type:"string",
+                            label:"City",
+                            name:"ctiy"
+                        },
+                        {
+                            type:"string",
+                            label:"State",
+                            name:"state"
+                        },
+                        {
+                            type:"string",
+                            label:"Zip Code",
+                            name:"zipCode"
+                        }
+                    ]
+                },
+                {
+                    type:"string",
+                    label:"Office Phone",
+                    name:"branchPhone"
+                },
 //Add Project manager  and other administrator information here. The information will be used not on the contact page but other related pages like Work at YEP, Work with YEP, etc...
-                ]
-            },
-        ],
-          ui: {
-    router: ({ document }) => {
-        return `/contact`;
+            ]
+        },
+    ],
+    ui: {
+        router: ({ document }) => {
+            return `/contacts/${document._sys.filename}`;
+        },
     },
-  },
 };
