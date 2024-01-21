6421bc90549eead79f19e679c73903ea40bd9237
Brennen Mazur <specdir@protonmail.ch>
2024-01-21 08:40:42 -0700

    Style: style adornment component and put on backburner

--- components/page_objects/adornment.tsx
+++ components/page_objects/adornment.tsx
@@ -8,14 +8,14 @@ export function Adornment(props: PageSectionAdornment) {
     const width = props.width;
     const xOffset = props.offsetX;
     const yOffset = props.offsetY;
+    let styleI = `absolute w-${width} h-${height} right-${xOffset} bottom-${yOffset}`
+    let styleD = `static`
     return(
-      <>
-        <article className="">  
-          <div className="">
-            <img src={props.adornmentImage || ""} data-tina-field={tinaField(props, "adornmentImage")} />
+        <article className="static z-40">  
+          <div className={styleD}> {styleI}
+            <img className={styleI} src={props.adornmentImage || ""} data-tina-field={tinaField(props, "adornmentImage")} />
           </div>
         </article>
-        {/* <pre>{ JSON.stringify(props, null, 2) }</pre> */}
-        </>
     )
 }
+{/* <pre>{ JSON.stringify(props, null, 2) }</pre> */}
--- tina/collections/page.js
+++ tina/collections/page.js
@@ -41,7 +41,7 @@ export default {
           contactForm,
           leftImage,
           rightImage,
-          adornment,
+//          adornment,
           timeline,
         ]
       }
