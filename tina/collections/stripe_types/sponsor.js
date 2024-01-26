f98d99aa254e9e6f62bd0bb4e991e5b2a67bd1a2
Nikolas <kolemazur@gmail.com>
2024-01-25 19:07:26 -0700

    Add Stripe collection types for  donations, products, and sponsors.

---
+++ tina/collections/stripe_types/sponsor.js
@@ -0,0 +1,49 @@
+export default {
+    name:"sponsor",
+    label: "Sponsor",
+    type:"object",
+    fields:[
+        {
+            name:"sponsorTitle",
+            label:"Sponsor Title",
+            description:"Product description",
+            type:"string",
+            isTitle:true,
+            required:true,
+        },
+        {
+            name:"image",
+            label:"Sponsor image",
+            type:"image",
+            required:false,
+        },
+        // {
+        //     name:"paymentType",
+        //     label:"Payment Type",
+        //     description:"Recurring or One-off",
+        //     type:"boolean",
+        //     required:true,
+        // },
+        {
+            name:"amount",
+            label:"Amount",
+            description:"$0.00",
+            type:"string",
+            required:true,
+        },
+        {
+            name:"billingPeriod",
+            label:"Billing Period",
+            description:"Timeline for recurring payment",
+            type:"string",
+            required:false,
+        },
+        {
+            name:"stripeLink",
+            label:"Stripe Payment Link",
+            description:"Link to Stripe pyament page",
+            type:"string",
+            required:true,
+        },
+    ]
+}
