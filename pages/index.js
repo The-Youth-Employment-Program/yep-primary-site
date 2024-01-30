dff715896b4adca4cb08f2802c7e9a5803cedcc7
Brennen Mazur <specdir@protonmail.ch>
2024-01-29 18:51:25 -0700

    Structure: add home page hero partner bar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { TinaMarkdown } from "tinacms/dist/rich-text";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
-import { tinaField, useTina } from "tinacms/dist/react";
+import { useTina } from "tinacms/dist/react";
 import { client } from "../tina/__generated__/client";
 import { HeroBanner } from "../components/page_objects/hero-banner";
 import { Feature } from "../components/page_objects/feature";
@@ -34,7 +33,28 @@ export default function Home(props) {
         {data.page.section?.map((part, i) =>{
           switch (part?.__typename) {
             case "PageSectionHeroBanner" : {
-              return <HeroBanner key={i} {...part} />
+              return (<><HeroBanner key={i} {...part} /> 
+                <section className="p-6 bg-salmon-green text-gray-100 rounded-b-lg">
+                  <div className="container p-4 mx-auto text-center">
+                    <h2 className="text-4xl font-bold">Partners with industry leaders</h2>
+                  </div>
+                  <div className="container flex flex-wrap justify-center mx-auto text-salmon-light-g2">
+                    <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
+                      <img src="/uploads/team/partners/Bureau-of-Land-Management/Logo_of_the_United_States_Bureau_of_Land_Management.svg" className="w-16 h-16" />
+                    </div>
+                    <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
+                      <img src="/uploads/team/partners/US-Forest-Service/usfs.png" className="w-16 h-16" />
+                    </div>
+                    <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
+                      <img src="/uploads/team/partners/The-Nature-Conservancy/the-nature-conservancy-logos-idAgi8uT3_.jpeg" className="w-16 h-16 rounded-full" />
+                    </div>
+                    <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
+                      <img src="/uploads/team/partners/National-Wildlife-Federation/national-wildlife-federation.png" className="w-16 h-16 rounded-full" />
+                    </div>
+                  </div>
+                </section>
+                </>
+              )
             }
             case "PageSectionFeatures" : {
               return <Feature key={i} {...part} />
