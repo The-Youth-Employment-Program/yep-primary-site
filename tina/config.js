b674d0a15188bf53f7bb0d5cd5472eae6d826d92
Brennen Mazur <specdir@protonmail.ch>
2024-02-10 23:05:40 +0900

    Fix Build, config file. Add pesky global css

--- tina/config.js
+++ tina/config.js
@@ -8,7 +8,7 @@ import stripeContent from "./collections/stripe-content"
 export const config = defineConfig({
   clientId: process.env.TINA_PUBLIC_CLIENT_ID || "",
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+    process.env.TINA_PUBLIC_BRANCH || // custom branch env override
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
     process.env.HEAD || "", // Netlify branch env
   token: process.env.TINA_TOKEN || "",
