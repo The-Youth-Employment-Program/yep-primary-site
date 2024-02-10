df71f67dadd2572a56f72c3f5618293678574b20
Brennen Mazur <specdir@protonmail.ch>
2024-02-10 21:20:00 +0900

    Fix footer git link, add staff member, add supporter images

--- components/YepFoot.js
+++ components/YepFoot.js
@@ -93,7 +93,7 @@ export const YepFoot = () => {
               <path d="M20.7 18.1c0 1.4-1.2 2.6-2.6 2.6H2.8c-1.4 0-2.6-1.2-2.6-2.6V2.8C.2 1.4 1.4.2 2.8.2h15.3c1.4 0 2.6 1.2 2.6 2.6v15.3zm-2.3-9.3h-1.8c.2.5.3 1.2.3 1.8 0 3.4-2.9 6.2-6.4 6.2-3.5 0-6.4-2.8-6.4-6.2 0-.6.1-1.2.3-1.8H2.5v8.7c0 .5.4.8.8.8h14.3c.5 0 .8-.4.8-.8V8.8zm-7.9-2.4c-2.3 0-4.1 1.8-4.1 4s1.8 4 4.1 4 4.1-1.8 4.1-4-1.8-4-4.1-4zm7.9-3c0-.5-.4-.9-.9-.9h-2.3c-.5 0-.9.4-.9.9v2.2c0 .5.4.9.9.9h2.3c.5 0 .9-.4.9-.9V3.4z"/>
             </svg>
           </a>
-          <a rel="noopener noreferrer" target="_blank" href="#" title="GitHub" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-salmon-green dark:text-salmon-light-g2">
+          <a rel="noopener noreferrer" target="_blank" href="https://github.com/The-Youth-Employment-Program/yep-primary-site" title="GitHub" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-salmon-green dark:text-salmon-light-g2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
               <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
             </svg>
