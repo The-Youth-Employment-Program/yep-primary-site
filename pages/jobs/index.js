1bc1ace321b98c546d5e857fd7fbfbeae611bba9
Brennen T. Mazur <yep@brennen.work>
2025-02-03 20:36:51 -0700

    bugfix: forgot job index changes

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -26,16 +26,15 @@ export default function JobsList(props) {
         <p className="mb-4">The Youth Employment Program is an innovative non-profit looking for candidates interested in dedicating themselves to the growth of this meaningful organization for at least two years! We make such a big impact on the landscape by doing conservation projects and by mentoring our employees. All candidates must be physically capable of working and teaching the very physical conservation techniques that are required by their position. We work in sweet wild landscapes! We get to camp out in the field most of the summer. Get in your public land fix. Seasonal positions have the possibility to expand into fulltime position or careers as we grow together.</p>
 
 {/* <div className="grid mx-2 sm:grid-cols-2 xl:grid-cols-4 sm:gap-4 md:gap-8 lg:gap-16"> */}
-<div className="grid mx-2 mb-4 sm:grid-cols-1 sm:gap-4 sm:mb-4 md:gap-8 md:mb-8 lg:gap-16">
+<div className="grid mx-2 mb-4 sm:grid-cols-1 sm:gap-4 sm:mb-4 md:gap-8 md:mb-8 lg:gap-14">
         
 {jobsList?.map((job,i) =>{
   if (job?.node?.active) {
           switch (job?.node?.__typename) {
             case "JobsFulltime" : {
-              {/* Todo: if active */}
               return (
                 <>
-                <div key={i} className="mt-4 pb-6 h-full rounded-lg shadow-md"> {/* grid md:grid-cols-2 pb-3 */}
+                <div key={i} className="pb-6 h-full rounded-lg shadow-md"> {/* grid md:grid-cols-2 pb-3 */}
                     <div className="relative p-0 m-0"> {/* data */}
                       <div className="flex justify-between align-middle py-4">   
                         <a href={`/jobs/${job?.node._sys.filename}` || "#"}>
@@ -50,32 +49,23 @@ export default function JobsList(props) {
                             {job.node.branch?.branch}
                           </div>
                       </div>
-                      <TinaMarkdown content={job.node.body} className="line-clamp-1" components={{
-                                  h1: props => <h1 className="hidden" {...props} />,
-                                  h2: props => <h2 className="hidden" {...props} />,
-                                  p: props => <p className="line-clamp-1 px-8" {...props} />,
-                                  li: props => <li className="hidden" {...props} />,
-                                  lic: props => <li className="hidden" {...props} />,
-                                  ul: props => <li className="hidden" {...props} />,
-                                  ol: props => <li className="hidden" {...props} />,
-                              }} />
+                      <p className="line-clamp-2 px-8">{job.node.brief}</p>
                       <a className="absolute right-0 flex items-center mt-4 mb-4 px-5 hover:text-salmon-tan text-salmon-red tracking-wider uppercase font-semibold text-lg" href={`/jobs/${job.node._sys.filename}` ||`#`}>Details
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="ml-2 w-5 h-5">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                           </svg>
                       </a> {/* this call doesn't work */}
-                      <div className="mb-4"></div>
+                      <div className="mb-6"></div>
                     </div>
                 </div>
                 </>
               )
             }
             case "JobsSeasonal" : {
-              {/* Todo: if active */}
               return (
                 <>
-                <div key={i} className="mt-4 pb-6 h-full rounded-lg shadow-md"> {/* grid md:grid-cols-2 pb-3 */}
-                    <div className="relative p-0 m-0"> {/* data */}
+                <div key={i} className="pb-6 h-full rounded-lg shadow-md"> {/* grid md:grid-cols-2 pb-3 */}
+                    <div className="relative pt-0 pb-2 m-0"> {/* data */}
                       <div className="flex justify-between align-middle py-4">   
                         <a href={`/jobs/${job?.node._sys.filename}` || "#"}>
                           <h2 className="ml-4 mt-2 align-middle text-xl font-medium text-salmon-dark-g1" data-tina-field={tinaField(jobsList[i].node,"title")}>
@@ -89,21 +79,13 @@ export default function JobsList(props) {
                             {job.node.branch?.branch}
                           </div>
                       </div>
-                      <TinaMarkdown content={job.node.body} className="line-clamp-1" components={{
-                                  h1: props => <h1 className="hidden" {...props} />,
-                                  h2: props => <h2 className="hidden" {...props} />,
-                                  p: props => <p className="line-clamp-1 px-8" {...props} />,
-                                  li: props => <li className="hidden" {...props} />,
-                                  lic: props => <li className="hidden" {...props} />,
-                                  ul: props => <li className="hidden" {...props} />,
-                                  ol: props => <li className="hidden" {...props} />,
-                              }} />
+                      <p className="line-clamp-2 px-8">{job.node.brief}</p>
                       <a className="absolute right-0 flex items-center mt-4 mb-4 px-5 hover:text-salmon-tan text-salmon-red tracking-wider uppercase font-semibold text-lg" href={`/jobs/${job.node._sys.filename}` ||`#`}>Details
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="ml-2 w-5 h-5">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                           </svg>
                       </a> {/* this call doesn't work */}
-                      <div className="mb-4"></div>
+                      <div className="mb-6"></div>
                     </div>
                 </div>
                 </>
@@ -111,7 +93,6 @@ export default function JobsList(props) {
             }
           }}
         })}
-
       </div>
       </main>
       <YepFoot />
