29b291bb04fc580f3aba4c90d295603e12a15ec0
Brennen T. M. <yep@brennen.work>
2026-04-16 19:18:23 -0600

    change statically typed contact e-addr

--- components/YepFoot.js
+++ components/YepFoot.js
@@ -77,7 +77,7 @@ export const YepFoot = () => {
           </a> */}
         </div>
         <div className="flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13">
-          <a rel="noopener noreferrer" href="mailto:youthemployment14@gmail.com" title="Email" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-salmon-green dark:text-salmon-light-g2">
+          <a rel="noopener noreferrer" href="mailto:youthemployment.cpas@gmail.com" title="Email" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-salmon-green dark:text-salmon-light-g2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
               <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
               <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
